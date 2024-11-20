d7d719f904c48e51b9005faa204e765cd01f61ec
S_Semchishin <threes322@yandex.ru>
2024-11-20 11:55:18 +0300

    Задание выполнено

---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+public class Cat {
+    public Cat() {
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj;
+    }
+}
